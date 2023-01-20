fac5fe421c78331b59bf4e68b40b673a034101cc
seohwan <seohwan91@gmail.com>
2023-01-20 22:10:26 +0900

    Fixed minor

--- script/cache_random.c
+++ script/cache_random.c
@@ -16,7 +16,7 @@ int x[CACHE_MAX]; /* array going to randomly access through */
 
 int main() {
     int register i, index;
-    int steps, csize;
+    int steps, csize, steps_all;
     double sec_temp, sec, WCET ;
 	struct timespec  begin, end;
 	long res;
@@ -27,6 +27,7 @@ int main() {
     for (csize=CACHE_MIN; csize <= CACHE_MAX; csize=csize*2){
 		sec = 0.0;
 		sec_temp = 0.0;
+		steps_all = 0;
 		WCET = -1.0;	
 		for(steps=0;steps<TSTEPS;steps++){
 			clock_gettime(CLOCK_MONOTONIC, &begin);
@@ -52,19 +53,24 @@ int main() {
 				continue;
 			}
 			res= res- ((end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec));
+			if(res < 0.0){
+				fprintf(fpw,"Testcase %d read+write: %4.4lf ms\n", steps, sec_temp);
+				continue;
+			}
 			if (res > WCET){
 				WCET = res;
 			}
 			//printf("%4.4lf %4.4lf\n",(double)((end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec))/1e6, (double)res/1e6);
 			sec = sec + res;
 			fprintf(fpw,"Testcase %d read+write: %4.4lf ms\n", steps, (double) res/1e6);
+			steps_all = steps_all + 1;
 		}
 		
-		printf("Size: %7ld Average read+write: %4.4lf ms WCET read+write: %4.4lf ms\n",
-	       csize*sizeof(int), (double) sec/1e6/TSTEPS, (double) WCET/1e6);
+		printf("Size: %7ld Average read+write: %4.4lf ms WCRT read+write: %4.4lf ms Error ratio %3.2lf%%\n",
+	       csize*sizeof(int), (double) sec/1e6/steps_all, (double) WCET/1e6, (double)(TSTEPS- steps_all)/(double)TSTEPS*100);
 
-		fprintf(fpw,"Size: %7ld Average read+write: %4.4lf ms WCET read+write: %4.4lf ms\n",
-	       csize*sizeof(int), (double) sec/1e6/TSTEPS, (double) WCET/1e6);
+		fprintf(fpw,"Size: %7ld Average read+write: %4.4lf ms WCRT read+write: %4.4lf ms Error ratio %3.2lf%%\n",
+	       csize*sizeof(int), (double) sec/1e6/steps_all, (double) WCET/1e6, (double)(TSTEPS- steps_all)/(double)TSTEPS*100);
 	}
 	
 }
